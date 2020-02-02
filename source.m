d86dfb5b4abdc3e8100495cb34458a259d2e17cd
phanibala <34336273+phanibala@users.noreply.github.com>
2020-02-02 12:06:12 +0530

    Update and rename fincod.m to source.m

--- source.m
+++ source.m
@@ -1,5 +1,4 @@
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-
+% Author : Phani Bala @2016
 % import input images
 clc;
 clear all;
